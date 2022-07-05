b732d921753363a4ddf6e7d6f52b2586cd716182
davidtarrab <dudutarrab@gmail.com>
2022-07-05 17:19:15 -0300

    clase 13 validacion incluida

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -3,39 +3,30 @@ import { MiContexto } from "../../context/CartContext"
 import {addDoc, collection, getFirestore} from "firebase/firestore"
 import "./CheckOut.css"
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
 
 
 const CheckOut = () => {
-    
+
     const {carrito, setCarrito, precios, setPrecios, setCantidadTotal} = useContext(MiContexto)
-    const [nombre, setNombre] = useState("")
-    const [apellido, setApellido] = useState("")
-    const [email, setEmail] = useState("")
-    const [ciudad, setCiudad] = useState("")
-    const [cp, setCp] = useState("")
+    const { register, formState: { errors } ,handleSubmit } = useForm() 
     const [finalizada, setFinalizada] = useState(false)
-    const [formIncompleto, setFormIncompleto] = useState (false)
     const [nID, setNID] = useState(undefined)
 
     const db = getFirestore()
     const orderCollection = collection(db, "orders")
 
-    const enviarPedido = () => {
-        if(nombre == ""){
-            setFormIncompleto(true)
-        }else{
-            const order = {
-                buyer: {nombre, apellido, email, ciudad, cp},
-                items: carrito,
-                total: precios
-            }
-            addDoc(orderCollection, order).then(({id})=>setNID(id))
+    const onSubmit = (buyer) =>{
+        const order = {
+            buyer,
+            items: carrito,
+            total: precios
+        }
+        addDoc(orderCollection, order).then(({id})=>setNID(id))
             setCarrito([])
             setCantidadTotal(0)
             setPrecios(0)
             setFinalizada(true)
-            setFormIncompleto(false)
-        }
     }
 
     return ( 
@@ -54,37 +45,49 @@ const CheckOut = () => {
                     <h5>Complete el formulario para terminar su compra</h5>
                 </div>
                 <div className="d-flex justify-content-center">
-                    <div className="row formControl">
+                    <form className="row formControl" onSubmit={handleSubmit(onSubmit)}>
                         <div className="col-md-4">
                             <label >Nombre</label>
-                            <input type="text" className="form-control"onChange={(e)=> setNombre(e.target.value)} required/>
+                            <input type="text" className="form-control" {...register("nombre", {
+                                required: true,
+                                maxLength: 15
+                            })}/>
+                            {errors.nombre?.type === "required" && <p className="text-danger">El campo nombre es obligatorio</p>}
                         </div>
                         <div className="col-md-4">
                             <label >Apellido</label>
-                            <input type="text" className="form-control"onChange={(e)=> setApellido(e.target.value)} required/>
+                            <input type="text" className="form-control" {...register("apellido", {
+                                required: true,
+                                maxLength: 20
+                            })}/>
+                            {errors.apellido?.type === "required" && <p className="text-danger">El campo apellido es obligatorio</p>}
                         </div>
                         <div className="col-md-4">
                             <label>Email</label>
-                            <input type="email" className="form-control"onChange={(e)=> setEmail(e.target.value)}/>
+                            <input type="email" className="form-control" {...register("email", {
+                                pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                            })}/>
+                            {errors.email?.type === "pattern" && <p className="text-danger">El formato del email es incorrecto</p>}
                         </div>
                         <div className="col-md-6">
                             <label >Ciudad</label>
-                            <input type="text" className="form-control"onChange={(e)=> setCiudad(e.target.value)}  required/>
+                            <input type="text" className="form-control" {...register("ciudad", {
+                                required: true
+                            })}/>
+                            {errors.ciudad?.type === "required" && <p className="text-danger">El campo ciudad es obligatorio</p>}
                         </div>
                         <div className="col-md-3">
                             <label>Codigo Postal</label>
-                            <input type="number" className="form-control"onChange={(e)=> setCp(e.target.value)}/>
+                            <input type="number" className="form-control" {...register("cp", {
+                                required: true,
+                                maxLength: 15
+                            })}/>
+                            {errors.cp?.type === "required" && <p className="text-danger">El campo codigo postal es obligatorio</p>}
                         </div>
-                        {
-                            formIncompleto? 
-                                <h3 className="text-danger">Formulario incompleto</h3>
-                            :
-                                ""
-                        }
                         <div className="col-12 mt-4">
-                            <button className="btn btn-dark" onClick={enviarPedido}>Terminar Compra</button>
+                            <button className="btn btn-dark" type="submit" >Terminar Compra</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
                 </>
             }
