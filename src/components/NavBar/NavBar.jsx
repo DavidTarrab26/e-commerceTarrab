b0c960dc8d66c65d0a9109c3cbf3adee5aef840d
Davidtarrab <dudutarrab@gmail.com>
2022-07-12 00:43:40 -0300

    nav responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,47 @@ const NavBar = () => {
     const {carrito} = useContext(MiContexto)
 
     return ( 
-        <nav className="navbar nav container-fluid d-flex flex-nowrap shadow">
+        <>
+        <nav className="navbar navbar-expand-lg nav shadow">
+            <div className="container-fluid">
+                <div>
+                    <Link to={'/'}><img src={logo} alt="logo" className= 'logoNav' /></Link>
+                    <h2 className= 'textLogo' >Ecommerce</h2>
+                </div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="bi bi-list"></i>
+                </button> 
+                <div className="collapse navbar-collapse contText" id="navbarNav">
+                    <ul className="navbar-nav contBotones">
+                        <li className="nav-item">
+                            <Link to={'/'} className="navbar-brand boton">Home</Link>
+                        </li>
+                        <div className="container-fluid contLiNormal">
+                            <li className="nav-item">
+                                <Link className="navbar-brand text-black textNav" to={'/category/remeras'}>Remeras</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="navbar-brand text-black textNav" to={'/category/pantalones'}>Pantalones</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="navbar-brand text-black textNav" to={'/category/camisas'}>Camisas</Link>
+                            </li>
+                        </div>
+                        <li className="nav-item">
+                            <Link to={'/'} className="navbar-brand boton">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {carrito.length > 0 ?
+            <div className="d-flex justify-content-end">
+                <Link className="text-black" to={'/cart'}><CartWidget /></Link>
+            </div>
+            :
+            ""
+            }
+        </nav>
+        {/* <nav className="navbar nav container-fluid d-flex flex-nowrap shadow">
             <div>
                 <Link to={'/'}><img src={logo} alt="logo" className= 'logoNav' /></Link>
                 <h2 className= 'textLogo' >Ecommerce</h2>
@@ -28,14 +68,9 @@ const NavBar = () => {
                     <button type="button" className="btn btn-dark radiusBotones">Login</button>
                 </div>
             </div>
-            {carrito.length > 0 ?
-            <div className="d-flex justify-content-end">
-                <Link className="text-black" to={'/cart'}><CartWidget /></Link>
-            </div>
-            :
-            ""
-            }
-        </nav>
+            
+        </nav> */}
+        </>
      );
 }
  
